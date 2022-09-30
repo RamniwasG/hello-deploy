fde0167f66ed62e575f0c55734fccbfc3bc10161
Ramniwas Gupta <ramniwas.nitrr15@gmail.com>
2022-10-01 01:03:39 +0530

    df

--- hello_deploy/settings.py
+++ hello_deploy/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'hello-msg-deploy.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
